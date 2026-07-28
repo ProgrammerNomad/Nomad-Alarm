2117331fd797cfbd5d4a7a992678a64688043bf6
Shiv Singh <shiv@srapsware.com>
2026-07-28 18:56:17 +0530

    Refactor adaptive icon and NomadLogo widget. Update adaptive icon foreground inset to 20% and modify NomadLogo to conditionally display a background with customizable padding and border radius.

--- lib/shared/widgets/nomad_logo.dart
+++ lib/shared/widgets/nomad_logo.dart
@@ -1,21 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:nomad_alarm/core/constants/app_constants.dart';
+import 'package:nomad_alarm/theme/app_colors.dart';
 
 class NomadLogo extends StatelessWidget {
   const NomadLogo({
     super.key,
     this.size = 120,
+    this.showBackground = true,
   });
 
   final double size;
+  final bool showBackground;
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(
+    final logo = Image.asset(
       AppConstants.logoAsset,
-      width: size,
-      height: size,
       fit: BoxFit.contain,
+    );
+
+    if (!showBackground) {
+      return SizedBox(
+        width: size,
+        height: size,
+        child: logo,
+      );
+    }
+
+    final padding = size * 0.18;
+    final borderRadius = BorderRadius.circular(size * 0.22);
+
+    return ClipRRect(
+      borderRadius: borderRadius,
+      child: Container(
+        width: size,
+        height: size,
+        color: AppColors.splashBackground,
+        padding: EdgeInsets.all(padding),
+        child: logo,
+      ),
     );
   }
 }
